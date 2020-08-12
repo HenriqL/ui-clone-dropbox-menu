4450fae55ab1d3db19fa393dcb4af14e1a8778ba
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-12 12:20:11 -0300

    structure creation

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+interface Props{
+    variant: 'blue' | 'beige' | 'white' | 'black';
+    title: string;
+    description: string
+}
+const Section: React.FC<Props> = () => {
+  return <div />;
+}
+
+export default Section;
